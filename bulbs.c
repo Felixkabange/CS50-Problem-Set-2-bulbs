5c7c10600a5de04457b57b56999b81bb5140b55b
Felix Kabange <kabangefelly@gmail.com>
2023-05-23 20:20:02 +0200

    Add bulbs.c

---
+++ bulbs.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    string user = get_string("What's your message?: ");
+    for (int i = 0, l = strlen(user); i < l; i++)
+    {
+        char ar = user[i];
+        int n;
+        for (n = 7; n >= 0; n--)
+        {
+            int bit = (ar >> n) & 1;
+            print_bulb(bit);
+            if (n == 0)
+            {
+                printf("\n");
+            }
+        }
+    }
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
